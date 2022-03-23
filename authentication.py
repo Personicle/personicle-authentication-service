88adf32a75649bd9e5f33a6a02766fbbaf9059a9
tirth-clearsense <tpatel@clearsense.com>
2022-03-23 10:19:20 -0500

    Validate accesstoken with user

--- authentication.py
+++ authentication.py
@@ -1,17 +1,19 @@
 from flask import Flask, jsonify, request
-from okta_authenticate.helpers import is_authorized 
+from okta_authenticate.helpers import is_authorized , is_user_valid
 
 app = Flask(__name__)
 
 @app.route('/authenticate',methods=["GET", "POST"])
 def authenticate():
-    if not is_authorized(request):
+    
+    if not is_user_valid(request) or not is_authorized(request):
         return "Unauthorized", 401
    
     return jsonify({"message": True})
 
 @app.route('/', methods=['GET','POST'])
 def index():
+    
     return jsonify({"message": "Personicle authentication server"})
 
 if __name__ == '__main__':
