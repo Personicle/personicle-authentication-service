97de1ff017c59c2599cf6d9ed84ab0501bbfc0b2
tirth-clearsense <tpatel@clearsense.com>
2022-04-01 15:31:22 -0500

    scope update

--- authentication.py
+++ authentication.py
@@ -1,6 +1,6 @@
 from distutils.log import debug
 from flask import Flask, jsonify, request
-from okta_authenticate.helpers import is_authorized, get_user_info, get_scopes, match_scopes
+from okta_authenticate.helpers import is_authorized, get_user_info, get_scopes, match_scopes, get_audience
 import os
 import collections
 
@@ -12,17 +12,20 @@ def authenticate():
     requested_scopes = request.args.get('scopes')
     default_scopes = ['openid','email','profile']
     requested_scopes_list = requested_scopes.split(",") if requested_scopes is not None else default_scopes
-
+    requested_scopes_list += default_scopes
+    # print(requested_scopes_list)
     if not is_authorized(request):
         return "Unauthorized", 401
 
-    user_id = get_user_info(request)
+    audience = get_audience(request)
+    user_id = get_user_info(request,audience)
     valid_scopes = get_scopes(request)
     valid_scopes_set = set(valid_scopes)
+
     requested_scopes_set = set(requested_scopes_list)
     scopes_matched = match_scopes(requested_scopes_set,valid_scopes_set)
-    if not scopes_matched:
-        return "You do not have access to the requested scopes", 401
+    if not scopes_matched and audience!='api://default':
+        return jsonify({"message": "INVALID_SCOPES"}),403
 
     return jsonify({"message": True, "user_id": user_id})
 
@@ -32,6 +35,6 @@ def index():
     return jsonify({"message": "Personicle authentication server"})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
     # os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
     # app.run(IDENTITY_SERVER_SETTINGS['HOST_URL'], port=IDENTITY_SERVER_SETTINGS['HOST_PORT'], debug=True)#, ssl_context='adhoc')
--- okta_authenticate/helpers.py
+++ okta_authenticate/helpers.py
@@ -8,6 +8,11 @@ loop = asyncio.get_event_loop()
 def get_token(request):
     return request.headers.get("Authorization").split("Bearer ")[1]
 
+def get_audience(request):
+    token = get_token(request)
+    decoded_token = jwt.decode(token, options={"verify_signature": False}, algorithms=["RS256"])
+    return decoded_token['aud']
+
 def get_scopes(request):
     token = get_token(request)
     decoded_token = jwt.decode(token, options={"verify_signature": False},algorithms=["RS256"])
@@ -19,10 +24,13 @@ def match_scopes(requested_scopes, valid_scopes):
             return False
     return True
 
-def get_user_info(request):
+def get_user_info(request,audience):
     token = get_token(request)
     headers = {'Authorization': 'Bearer ' + token}
-    user_info = requests.get(config["userinfo_uri"], headers=headers)
+    if audience == "thirdparty":
+        user_info = requests.get(config["client_userinfo_uri"], headers=headers)
+    else:
+        user_info = requests.get(config["userinfo_uri"], headers=headers)
     real_user_id = user_info.json()['sub']
     
     return real_user_id
@@ -30,13 +38,18 @@ def get_user_info(request):
 def is_authorized(request):
     try:    
         token = get_token(request)
-        return is_access_token_valid(token, config["issuer"])
+        audience  = get_audience(request)
+
+        if audience == 'thirdparty':
+            return is_access_token_valid(token, config["client_issuer"],audience)
+
+        return is_access_token_valid(token, config["issuer"],audience)
     except Exception:
         return False
 
 
-def is_access_token_valid(token, issuer):
-    jwt_verifier = AccessTokenVerifier(issuer=issuer, audience='api://default')
+def is_access_token_valid(token, issuer,audience):
+    jwt_verifier = AccessTokenVerifier(issuer=issuer, audience=audience)
     try:
         loop.run_until_complete(jwt_verifier.verify(token))
         return True
